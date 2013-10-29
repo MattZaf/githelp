f694d9227db2eb6f6f0136a18e96f029bd45087d
Matt Zafirovski <mattzaf@gmail.com>
2013-10-29 16:54:54 -0500

    Created rails app

---
+++ githelp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Githelp::Application.config.secret_key_base = 'a7e3ebae24d2430569db754f74285d8e10399c5ae75aa42232aae3d41195b5431f31ef3277b9e4268fa6211441ed29ac84d0fef17536530cdf90aa53d5b09fd6'
